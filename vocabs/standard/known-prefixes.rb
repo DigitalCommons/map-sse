bde8b13cbae0a38b408c9eeba7915bd0a4c398e4
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-01-19 21:45:58 +0000

    vocabs/standard/known-prefixes.rb - add essglobal, coun, reg, sreg

--- vocabs/standard/known-prefixes.rb
+++ vocabs/standard/known-prefixes.rb
@@ -9,9 +9,13 @@ KnownPrefixes = {
   rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
   
   ossr: "http://data.ordnancesurvey.co.uk/ontology/spatialrelations/", # for within
-# essglobal: this varies... can't include it here. Can we?
-# reg:  depends on essglobal:super-regions-ica
-# sreg: depends on essglobal:regions-ica
+  
+  # These vary depending on dev/prod... but are fixed up at a later stage
+  # after the ttls are generated.
+  essglobal: "http://purl.org/essglobal/standard/",
+  coun: "http://purl.org/essglobal/standard/countries-iso/",
+  reg: "http://purl.org/essglobal/standard/regions-ica/",
+  sreg: "http://purl.org/essglobal/standard/super-regions-ica/",
 }
 
   
